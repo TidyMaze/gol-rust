549a847d5c348d391d0543a82d482a0951bcdc1c
Yann Rolland <groskiff@gmail.com>
2021-03-13 22:36:35 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,18 @@ const HEIGHT: usize = 200;
 const WIDTH: usize = 400;
 type Grid = [[bool; WIDTH]; HEIGHT];
 
-// fn show_bool(b: &bool) -> &str {
-//     return if *b { "O" } else { "." };
-// }
+fn show_bool(b: &bool) -> &str {
+    return if *b { "O" } else { "." };
+}
 
-// fn print_grid(grid: Grid) {
-//     let lines = grid
-//         .iter()
-//         .map(|l| l.iter().map(show_bool).collect::<Vec<&str>>().join(""))
-//         .collect::<Vec<String>>()
-//         .join("\n");
-//     println!("{}", lines);
-// }
+fn print_grid(grid: &Grid) {
+    let lines = grid
+        .iter()
+        .map(|l| l.iter().map(show_bool).collect::<Vec<&str>>().join(""))
+        .collect::<Vec<String>>()
+        .join("\n");
+    println!("{}", lines);
+}
 
 fn dead_or_alive(alive: bool, neighbors: u8) -> bool {
     return (alive && neighbors >= 2 && neighbors <= 3) || (!alive && neighbors == 3);
@@ -32,7 +32,7 @@ const OFFSETS: [(i16, i16); 8] = [
     (0 + 1, 0 + 1),
 ];
 
-fn count_neighbors(grid: Grid, j: usize, i: usize) -> u8 {
+fn count_neighbors(grid: &Grid, j: usize, i: usize) -> u8 {
     let mut cnt = 0;
     for (o_j, o_i) in &OFFSETS {
         let dj: i16 = j as i16 + o_j;
@@ -50,7 +50,7 @@ fn count_neighbors(grid: Grid, j: usize, i: usize) -> u8 {
     return cnt;
 }
 
-fn one_step(grid: Grid) -> Grid {
+fn one_step(grid: &Grid) -> Grid {
     let mut new_grid: Grid = [[false; WIDTH]; HEIGHT];
     for i in 0..HEIGHT {
         for j in 0..WIDTH {
@@ -74,8 +74,8 @@ fn main() {
     let init = make_rand_grid();
     let mut g = init;
     for i in 0..100 {
-        g = one_step(g);
+        g = one_step(&g);
         println!("==================== - {}", i);
-        // print_grid(g)
+        // print_grid(&g)
     }
 }
