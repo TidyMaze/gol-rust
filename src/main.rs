560f4ceecfc1e8e5a0d54903fd97f096bd3849d7
Yann Rolland <groskiff@gmail.com>
2021-03-14 20:12:48 +0100

    window

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use rand::prelude::*;
+use macroquad::prelude::rand::gen_range;
+use macroquad::prelude::*;
 
-const HEIGHT: usize = 200;
-const WIDTH: usize = 400;
-type Grid = [[bool; WIDTH]; HEIGHT];
+type Grid = Vec<Vec<bool>>;
 
 fn show_bool(b: &bool) -> &str {
     return if *b { "O" } else { "." };
@@ -38,9 +37,9 @@ fn count_neighbors(grid: &Grid, j: usize, i: usize) -> u8 {
         let dj: i16 = j as i16 + o_j;
         let di: i16 = i as i16 + o_i;
         if dj >= 0
-            && dj < WIDTH as i16
+            && dj < grid[0].len() as i16
             && di >= 0
-            && di < HEIGHT as i16
+            && di < grid.len() as i16
             && grid[di as usize][dj as usize]
         {
             cnt += 1;
@@ -51,31 +50,53 @@ fn count_neighbors(grid: &Grid, j: usize, i: usize) -> u8 {
 }
 
 fn one_step(grid: &Grid) -> Grid {
-    let mut new_grid: Grid = [[false; WIDTH]; HEIGHT];
-    for i in 0..HEIGHT {
-        for j in 0..WIDTH {
-            new_grid[i][j] = dead_or_alive(grid[i][j], count_neighbors(grid, j, i));
+    let mut new_grid: Grid = Vec::new();
+    for i in 0..grid.len() {
+        let mut line: Vec<bool> = Vec::new();
+
+        for j in 0..grid[0].len() {
+            line.push(dead_or_alive(grid[i][j], count_neighbors(grid, j, i)));
         }
+        new_grid.push(line);
     }
     return new_grid;
 }
 
-fn make_rand_grid() -> Grid {
-    let mut res = [[false; WIDTH]; HEIGHT];
-    for i in 0..HEIGHT {
-        for j in 0..WIDTH {
-            res[i][j] = random();
+fn make_rand_grid(height: usize, width: usize) -> Grid {
+    let mut res: Grid = Vec::new();
+    for i in 0..height {
+        let mut line: Vec<bool> = Vec::new();
+
+        for j in 0..width {
+            line.push(gen_range(0, 101) >= 50);
         }
+        res.push(line);
     }
     return res;
 }
 
-fn main() {
-    let init = make_rand_grid();
+#[macroquad::main("BasicShapes")]
+async fn main() {
+    let height = screen_height() as u32;
+    let width = screen_width() as u32;
+
+    let init = make_rand_grid(height as usize, width as usize);
     let mut g = init;
-    for i in 0..100 {
+
+    for i in 0..10000 {
+        clear_background(BLACK);
         g = one_step(&g);
         println!("==================== - {}", i);
         // print_grid(&g)
+
+        for i in 0..height as usize {
+            for j in 0..width as usize {
+                if g[i][j] {
+                    draw_rectangle(j as f32, i as f32, 1 as f32, 1 as f32, BLUE);
+                }
+            }
+        }
+
+        next_frame().await
     }
 }
